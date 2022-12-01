a4649071837c1b429aac14d2957d612fbaa6ffa2
jekaterinq <119529477+jekaterinq@users.noreply.github.com>
2022-12-01 13:08:09 +0200

    Create solvedproblem.c

---
+++ solvedproblem.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+//#include <string.h>
+
+#define MAX 15
+#define MAX_LEN 20
+
+int main()
+{
+    //float time;
+    //int speed = 5;
+    int num;
+
+    do
+    {
+        printf("Sisesta kui palju teed meil on (MAX 15)?");
+        scanf("%d", &num);
+        if(num > MAX)
+        {
+            printf("(MAX 15!!!) Sisesta kui palju teed meil on?");
+            scanf("%d", &num);
+        }
+    }
+    while (num > MAX);
+
+    char roads[num][MAX_LEN];
+    double lenght[num];
+
+    int i;
+
+    for (i = 0; i < num; i++)
+    {
+        printf("Milline %d. teed nimetatud ja kui palju km see on?\n", i + 1);
+        scanf("%s", roads[i]);
+        scanf("%lf", &lenght[i]);
+    }
+
+    for (i = 0; i < num; i++)
+    {
+        printf("%s ", roads[i]);
+        printf("%lf km\n", lenght[i]);
+    }
+
+
+    return 0;
+}
